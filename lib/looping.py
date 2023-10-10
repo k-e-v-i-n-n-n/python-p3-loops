9c6524fe9a0776d8fda8e61ef1f128e98469d3d4
Kevin Breid <kevinbreid@yahoo.com>
2023-10-10 17:53:47 +0200

    practicing loops in python

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
+    
+
+int_list = [1, 2, 3, 4, 5]
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [num * num for num in int_list]
+    return int_list
+    
+# Write a function fizzbuzz() that prints the numbers from 1 to 100. 
+# For multiples of three, print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+# For numbers which are multiples of both three and five, print "FizzBuzz".
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+
+    while i <= 100:
+        if i % 3 ==0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+    
